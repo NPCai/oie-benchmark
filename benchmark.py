653d3a19b893b96657bf73826e8be19dd061c29b
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-07-30 19:15:16 -0400

    added reader for opennmt verbose format

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,6 @@
 ''' 
 Usage:
-   benchmark --gold=GOLD_OIE --out=OUTPUT_FILE (--stanford=STANFORD_OIE | --ollie=OLLIE_OIE |--reverb=REVERB_OIE | --clausie=CLAUSIE_OIE | --openiefour=OPENIEFOUR_OIE | --props=PROPS_OIE)
+   benchmark --gold=GOLD_OIE --out=OUTPUT_FILE (--stanford=STANFORD_OIE | --ollie=OLLIE_OIE |--reverb=REVERB_OIE | --clausie=CLAUSIE_OIE | --openiefour=OPENIEFOUR_OIE | --props=PROPS_OIE | --neural=NEURAL_OIE)
 
 Options:
   --gold=GOLD_OIE              The gold reference Open IE file (by default, it should be under ./oie_corpus/all.oie).
@@ -11,6 +11,7 @@ Options:
   --props=PROPS_OIE            Read PropS format from file PROPS_OIE
   --reverb=REVERB_OIE          Read ReVerb format from file REVERB_OIE
   --stanford=STANFORD_OIE      Read Stanford format from file STANFORD_OIE
+  --neural=NEURAL_OIE          Read Neural format from file NEURAL_OIE
 '''
 import docopt
 import string
@@ -26,6 +27,7 @@ from oie_readers.reVerbReader import ReVerbReader
 from oie_readers.clausieReader import ClausieReader
 from oie_readers.openieFourReader import OpenieFourReader
 from oie_readers.propsReader import PropSReader
+from oie_readers.opennmtReader import NeuralReader
 
 from oie_readers.goldReader import GoldReader
 from matcher import Matcher
@@ -175,6 +177,10 @@ if __name__ == '__main__':
     if args['--openiefour']:
         predicted = OpenieFourReader()
         predicted.read(args['--openiefour'])
+    
+    if args['--neural']:
+        predicted = NeuralReader()
+        predicted.read(args['--neural'])
 
     b = Benchmark(args['--gold'])
     out_filename = args['--out']
--- eval.sh
+++ eval.sh
@@ -6,5 +6,6 @@ python benchmark.py --gold=./oie_corpus/all.oie --out=eval/ReVerb.dat --reverb=.
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/ClausIE.dat --clausie=./systems_output/clausie_output.txt
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/OpenIE-4.dat --openiefour=./systems_output/openie4_output.txt
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/PropS.dat --props=./systems_output/props_output.txt
+python benchmark.py --gold=./oie_corpus/all.oie --out=eval/Neural.dat --neural=./systems_output/neural_output.txt
 python pr_plot.py --in=./eval --out=./eval/eval.png
 echo "DONE"
